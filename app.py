3b3d9474f5ade5be7c881fb9a42c486f6839fab3
Ben <ben.stoffelen@hm.edu>
2017-11-06 22:22:26 +0100

    BST: added initial create page

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
 
 if __name__ == '__main__':
     app.run()
