7e1e488172477d2f3fbc4beb5f3b003d7a7db7c8
Ben <ben.stoffelen@hm.edu>
2017-11-07 21:46:44 +0100

    BST: tweaked stories list and fixed some issues. added font awesome to static files

--- static/js/listfunctions.js
+++ static/js/listfunctions.js
@@ -5,13 +5,19 @@ $(document).ready(function () {
 
         var userInput = $("#new-item").val();
         console.log("User inputted...", userInput);
-
-        if (!userInput) {
-            $('.error').toggle();
-            $('.error').text('Please enter a story number!').show();
-        }
-        else {
-            $('#list-items').append("<li class='list-group-item'>" + userInput + "<i class='fa fa-trash fa-4'></i>" + "</li>");
+        /*
+         //Comment back in if errors should be thrown when entering empty and comment out the following block
+         if (!userInput) {
+         $('.error').collapse("show");
+         $('.error').text('Please enter a story number!').show();
+         }
+         else {
+         console.log('check');
+         $('#list-items').append("<li class='list-group-item'>" + userInput + "<i class='fa fa-trash fa-4'></i>" + "</li>");
+         }
+         */
+        if (userInput) {
+            $('#list-items').append("<li class='list-group-item'>" + userInput + "<i class='fa fa-trash float-right' aria-hidden='true'></i>" + "</li>");
         }
 
         $('#new-item').val("");
@@ -20,14 +26,16 @@ $(document).ready(function () {
     //end on click
 
     //remove list item on click of trash icon
-    $(".list-items").on("click", ".fa-trash", function () {
+    $("#list-items").on("click", ".fa-trash", function () {
         $(this).closest("li").remove();
         console.log("Removing item", $(this).closest("li"));
     });
 
-    //remove error message on focusing the input
-    $("#new-item").val('').focus(function () {
-        $('.error').toggle();
-    });
-
+    /*
+     //Comment back in if errors should be thrown when entering empty
+     //remove error message on focusing the input
+     $("#new-item").val('').focus(function () {
+     $('.error').collapse("hide");
+     });
+     */
 });
