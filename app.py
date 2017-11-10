905e540760bd771d5b3f5ff3d5dd509b516f9917
Ben <ben.stoffelen@hm.edu>
2017-11-10 23:16:53 +0100

    BST: added creation page for planning creation

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+    render_template, flash
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def start():
     return render_template('index.html')
+
 
 @app.route('/create')
 def create():
     return render_template('create.html')
+
+
+@app.route('/create_submit', methods=['POST'])
+def create_planning():
+    # TODO implement creation logic
+    planning_id = 'a408a018-3bfb-4eaf-943d-2c5340f10a02';
+    return render_template('created.html', planning_id=planning_id);
 
 
 if __name__ == '__main__':
