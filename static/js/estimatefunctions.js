be2b74c0c0c7d536a65206ba6d96b7cdb8d43823
Ben <ben.stoffelen@hm.edu>
2018-03-23 22:05:38 +0100

    BST: added actual save to db for estimations

--- static/js/estimatefunctions.js
+++ static/js/estimatefunctions.js
@@ -10,4 +10,30 @@ $(document).ready(function () {
         var id = "#" + this.id;
         $(id + "_cell").html(value);
     });
+    $('#submit_estimates').on('click', function () {
+        var global_data = $('#global-data').data();
+        var result_list = [];
+        $('#estimates_table > tbody > tr').each(function () {
+            $this = $(this);
+            var data = $this.find('td');
+            var storyid = data[0].innerHTML;
+            var storyname = data[1].innerHTML;
+            var estimate = data[2].innerHTML;
+            var comment = data[3].innerHTML;
+            result_list.push({storyid: storyid, storyname: storyname, estimate: estimate, comment: comment});
+        });
+        var json_data = JSON.stringify({data: result_list, user: global_data.username});
+        $.ajax({
+            url: '/save',
+            data: json_data,
+            contentType: 'application/json;charset=UTF-8',
+            type: 'POST',
+            success: function (response) {
+                window.location= "/saved?planning=" + global_data.planning
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
 });
